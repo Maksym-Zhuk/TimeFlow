55f8418bf1a5a2880a2c484131e37524bd5048e8
Maksym Zhuk <maksymzhuk759@gmail.com>
2025-08-17 15:04:09 +0300

    configure main file

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,21 +1,62 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import fastifyCookie from '@fastify/cookie';
+import fastifyHelmet from '@fastify/helmet';
+import fastifyCsrfProtection from '@fastify/csrf-protection';
+import fastifyCompress from '@fastify/compress';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
   );
+  await app.register(fastifyCookie, {
+    secret: process.env.COOKIE_SECRET as string,
+  });
+  await app.register(fastifyHelmet, {
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: [`'self'`, 'unpkg.com'],
+        styleSrc: [
+          `'self'`,
+          `'unsafe-inline'`,
+          'cdn.jsdelivr.net',
+          'fonts.googleapis.com',
+          'unpkg.com',
+        ],
+        fontSrc: [`'self'`, 'fonts.gstatic.com', 'data:'],
+        imgSrc: [`'self'`, 'data:', 'cdn.jsdelivr.net'],
+        scriptSrc: [
+          `'self'`,
+          `https: 'unsafe-inline'`,
+          `cdn.jsdelivr.net`,
+          `'unsafe-eval'`,
+        ],
+      },
+    },
+  });
+  await app.register(fastifyCsrfProtection);
+  await app.register(fastifyCompress, {
+    global: true,
+    zlibOptions: { level: 6 },
+    encodings: ['gzip', 'deflate', 'br'],
+    threshold: 1024,
+  });
+  app.enableCors({
+    origin: [''],
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
+  const port = process.env.PORT as string;
+  await app.listen(port);
+  Logger.log(`🚀 Application is running on: http://localhost:${port}`);
 }
 
 bootstrap();
