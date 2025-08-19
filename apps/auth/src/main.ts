d14bf6523c668bcc816ad1463d97f1a71d480cc3
Maksym Zhuk <maksymzhuk759@gmail.com>
2025-08-19 21:03:48 +0300

    configure  main auth file

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,21 +1,35 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
   );
+  const port = process.env.AUTH_PORT!;
+  await app.listen(port);
+  Logger.log(`🚀 Application is running on: http://localhost:${port}`);
+
+  const microservice =
+    await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+      transport: Transport.RMQ,
+      options: {
+        urls: [process.env.RABBITMQ_URL as string],
+        queue: 'auth_queue',
+        queueOptions: {
+          durable: true,
+        },
+      },
+    });
+
+  await microservice.listen();
+  Logger.log(`🚀Auth microservice is listening`);
 }
 
 bootstrap();
